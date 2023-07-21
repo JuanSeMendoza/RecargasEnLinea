fc1522de259bc262e379e802d2f9715049d44b29
MELEXA\jsmendoza <sebastian.mendoza@melexa.com>
2023-07-21 17:59:12 -0500

    Se hizo el controlador que funcione junto con los modelos

---
+++ src/main/java/com/datacenter/repository/RecargaDAO.java
@@ -0,0 +1,9 @@
+package com.datacenter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.datacenter.model.Tbl_Recargas;
+
+public interface RecargaDAO extends JpaRepository<Tbl_Recargas, Integer>{
+
+}
